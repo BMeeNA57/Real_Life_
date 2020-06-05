bf6798f9efe7b4bbb159011cb915b02cb3c655b0
Benjamin Meena <59247187+BMeeNA57@users.noreply.github.com>
2020-06-04 19:53:18 -0600

    updated birders and styling

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -27,10 +27,59 @@ function validateForm() {
       return false;
   }
   document.querySelector('.status').innerHTML = "Sending...";
-}
+}   
 /*End Booking Form*/
+function bigImg(x) {
+    x.style.height = "250px";
+    x.style.width = "350px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "250px";
+  }
+/*Index Page - Time*/
+var d = new Date();
+document.getElementById("timeIndex").innerHTML = d;
+/*End Index Page - Time*/
 
-/*Lightbox*/
-
-
-/*End Lightbox*/
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
