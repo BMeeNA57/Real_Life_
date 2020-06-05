fb286c70206f1c1792a39ec849ec19b8326f4c91
Benjamin Meena <59247187+BMeeNA57@users.noreply.github.com>
2020-06-05 16:29:13 -0600

    Changed color scheme and added photo gallery

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -43,43 +43,13 @@ var d = new Date();
 document.getElementById("timeIndex").innerHTML = d;
 /*End Index Page - Time*/
 
-// Open the Modal
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  
-  // Close the Modal
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+//*Lighbox Photo Page*//
+function startLightBox(){
+  var lbBg = document.getElementById("lightBoxBg");
+  var lb = document.getElementById("lightBox");
+
+  lbBg.style.display = "block";
+  lb.style.display = "block";
+}
+
+//*End Lightbox Photo Page*//
